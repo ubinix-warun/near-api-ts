012ab03d504095a98d93b24d3f691e1e4cd7649d
ubinix-warun <warun@ubinix.com>
2025-07-17 11:48:41 +0700

    refactor orval configuration and add local API generation script

--- orval.config.ts
+++ orval.config.ts
@@ -1,24 +1,42 @@
 import { defineConfig } from 'orval';
 
+console.log('orval.config.ts: Starting configuration for orval...');
+
+const OPENAPI_TARGETS: Record<string, string> = {
+    local: './nearcore-repo/chain/jsonrpc/openapi/openapi.json',
+    main: 'https://raw.githubusercontent.com/near/nearcore/master/chain/jsonrpc/openapi/openapi.json',
+    dev: 'https://raw.githubusercontent.com/ubinix-warun/nearcore/refs/heads/master/chain/jsonrpc/openapi/openapi.json',
+};
+
+const OPENAPI_OUTPUT_CLIENT_PATH = 'packages/jsonrpc-client';
+const OPENAPI_OUTPUT_TYPE_PATH = 'packages/jsonrpc-types';
+const OPENAPI_BASE_API_GENERATED_PATH = '/api/generated';
+// const OPENAPI_BASE_API_GENERATED_PATH = '/api/generated2';
+
+console.log('orval.config.ts:', OPENAPI_TARGETS[process.env.OPENAPI_ENV || 'dev']);
+
 export default defineConfig({
     nearApi: {
         input: {
-            // target: './nearcore-repo/chain/jsonrpc/openapi/openapi.json',
-            // target: 'https://raw.githubusercontent.com/near/nearcore/master/chain/jsonrpc/openapi/openapi.json',
-            target: 'https://raw.githubusercontent.com/ubinix-warun/nearcore/refs/heads/master/chain/jsonrpc/openapi/openapi.json'
+            target: OPENAPI_TARGETS[process.env.OPENAPI_ENV || 'dev']
         },
         output: {
             mode: 'split',
-            target: 'packages/jsonrpc-client/src/api/generated',
-            schemas: 'packages/jsonrpc-types/src/api/generated/model',
+            target: OPENAPI_OUTPUT_CLIENT_PATH + '/src' + OPENAPI_BASE_API_GENERATED_PATH,
+            schemas: OPENAPI_OUTPUT_TYPE_PATH + '/src' + OPENAPI_BASE_API_GENERATED_PATH + '/model',
             namingConvention: 'camelCase',
             client: 'react-query',
             httpClient: 'axios',
             override: {
                 mutator: {
-                    path: 'packages/jsonrpc-client/src/client.ts',
+                    path: OPENAPI_OUTPUT_CLIENT_PATH + '/src/client.ts',
                     name: 'customInstance',
                 },
+
+                // TS2395: Individual declarations in merged declaration useQuery must be all exported or all local.
+                // query: {
+                //     useQuery: false,
+                // },
             },
         },
         hooks: {
@@ -28,23 +46,15 @@ export default defineConfig({
 
     nearApiZod: {
         input: {
-            target: 'https://raw.githubusercontent.com/near/nearcore/master/chain/jsonrpc/openapi/openapi.json',
+            target: OPENAPI_TARGETS[process.env.OPENAPI_ENV || 'dev']
         },
         output: {
             mode: 'split',
-            target: 'packages/jsonrpc-types/src/api/generated',
-            // schemas: 'packages/client/src/api/generated/model',
+            target: OPENAPI_OUTPUT_TYPE_PATH +'/src' + OPENAPI_BASE_API_GENERATED_PATH,
             fileExtension: '.zod.ts',
             client: 'zod',
 
             // ERROR 359 -- { V2: [object Object] };export
-            // httpClient: 'axios',
-            // override: {
-            //     mutator: {
-            //         path: 'packages/client/src/api/client.ts',
-            //         name: 'customInstance',
-            //     },
-            // },
 
         }
     },
